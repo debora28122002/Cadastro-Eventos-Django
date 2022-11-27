c738733114dadd3b19be28e970219cf15786392d
Débora de Jesus <dbrdejesus15@gmail.com>
2022-11-27 13:05:40 -0200

    Projeto Django

--- app/models.py
+++ app/models.py
@@ -14,3 +14,6 @@ class Evento (models.Model):
     
     def __str__(self):
         return self.titulo
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
